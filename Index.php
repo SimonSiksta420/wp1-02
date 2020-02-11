3c07a5c1d7d2be4a61381053529a61bd5eb644c6
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-02-11 09:36:20 +0100

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,30 @@
+<?php
+$name = "Šimon Siksta";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Informace o mně </title>
+</head>
+<body>
+    <h2><?php echo $name; ?></h2>
+<ul>
+    <li>Narodil jsem se v roce 2003</li>
+    <li>Jsem muž</li>
+    <li>Gymnázium Čáslav</li>
+    <li>simon.siksta@student.ossp.cz</li>
+    <li>420 666 6969</li>
+    <li>Rád hraju videohry</li>
+    <li>Mám rád zvířata</li>
+    <li>Vlastním zakrslého králíka, který se jmenuje Šimek</li>
+    <li>Politika mě nezajímá</li>
+    <li>Baví mě čtení</li>
+    <li>Rád se učím nové jazyky</li>
+    <li>Umím velice dobře anglicky</li>
+    <li>Konec</li>
+</ul>
+</body>
+</html>
